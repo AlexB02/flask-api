2ba30ab05f4885ac6920a1fe5b78fb7505bc3b03
AlexB.02 <alexanderdb8@gmail.com>
2022-05-22 21:56:13 +0100

    feat: add requirements

--- api/main.py
+++ api/main.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-  return "index"
+  return jsonify("index")
 
 if __name__ == "__main__":
   app.run()
